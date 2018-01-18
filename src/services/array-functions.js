5279f70602a2845d3059b77cb4885793d6e59b13
Mark Hinojosa <markhinojosa1212@gmail.com>
2018-01-18 17:12:23 -0600

    edited

--- src/services/array-functions.js
+++ src/services/array-functions.js
@@ -1,26 +1,39 @@
 
 //in the function map, create a new array and store in a variable
-//loop theArray and call the fnc for each thing in the array, 
+//loop theArray and call the fnc for each thing in the array,
 //     passing in the item from the current loop into the call to fnc
 //add the returned value from fnc to the new array
 //return the new array
 export function map(theArray, fnc){
+  let newArr = [];
+  for( let i = 0; i < theArray.length; i++){
+    newArr.push(fnc(theArray[i]))
 
+  }
+  return newArr;
 }
 
 //create a new array
-//loop theArray and call the fnc for each thing in the array, 
+//loop theArray and call the fnc for each thing in the array,
 //     passing in the item from the current loop
 //fnc will return true or false, if true add the item to the new array else do not
 //return the new array
 export function filter(theArray, fnc){
 
+    for( let i = 0; i < theArray.length; i++){
+      newArr2 = [];
+      if(fnc(theArray[i])){
+        newArr2.push(theArray[i])
+      }
+        return newArr2;
+  }
+
 }
 
 
-//loop theArray and call the fnc for each thing in the array, 
+//loop theArray and call the fnc for each thing in the array,
 //     passing in the item from the current loop
-//fnc will return true or false, if true return the item 
+//fnc will return true or false, if true return the item
 //return null
 export function find(theArray, fnc){
 
